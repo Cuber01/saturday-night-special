8929b3063beaf9849a9d96b2d543162a07eb8604
Cuber01 <johnmallard631@gmail.com>
2023-12-13 20:08:23 +0100

    added 2 levels

--- src/LevelManager.gd
+++ src/LevelManager.gd
@@ -1,12 +1,36 @@
 extends Node2D
 
 var levels: Array
+var rng = RandomNumberGenerator.new()
+const PATH = "res://scenes/levels/"
+
+var load_next_level = true
 
 func _ready() -> void:
-	pass
+	load_levels()
+
+func load_levels() -> void:
+	var dir = Directory.new()
+	dir.open(PATH)
+	dir.list_dir_begin()
+	
+	while true:
+		var filename = dir.get_next()
+		if filename == "": 
+			break # break the loop if there are no more files left
+		elif !filename.begins_with("."):
+			#get_next() returns a string so this can be used to load the images into an array.
+			levels.append(load(PATH + filename))
+	dir.list_dir_end()
 
 func process() -> void:
-	pass
+	if load_next_level:
+		load_level(rng.randi_range(0, levels.size()))
+		load_next_level = false
+		
+	if Input.is_action_just_pressed("debug_button"):
+		load_next_level = true
+
 	
 func load_level(index: int) -> void:
 	pass
