0d6c7b53e00d71fcade4e1fcbf7d800362148388
Cuber01 <johnmallard631@gmail.com>
2023-12-19 09:59:17 +0100

    Further work on platforms

--- src/Player.gd
+++ src/Player.gd
@@ -6,7 +6,7 @@ export var facing_right: bool = true
 
 # Movement Constants
 const SPEED: int = 100
-const JUMP_FORCE: int = 175
+const JUMP_FORCE: int = 200
 const GRAVITY_FORCE: int = 275
 
 # Movement vars
