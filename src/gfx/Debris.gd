156b0e3d61944604aa6f7254e305c5d4639bfba5
Cuber01 <johnmallard631@gmail.com>
2024-01-18 20:40:36 +0100

    Debris tweaks

--- src/gfx/Debris.gd
+++ src/gfx/Debris.gd
@@ -4,6 +4,7 @@ func init(pos: Vector2) -> void:
 	global_position = pos
 
 func _on_Debris_body_entered(body: Node) -> void:
+	$Sprite.frame = 1
 	$Sprite.playing = true
 
 func _on_Sprite_animation_finished() -> void:
