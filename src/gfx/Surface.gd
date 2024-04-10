0bfa5f6f567b00b758d82927ef523405905d1ca2
Cuber01 <johnmallard631@gmail.com>
2024-04-10 16:44:21 +0200

    Trying to debug blood

--- src/gfx/Surface.gd
+++ src/gfx/Surface.gd
@@ -1,9 +1,6 @@
 extends Sprite
-class_name paint
 
 var surface_image: Image = Image.new()
-var surface_texture: ImageTexture = ImageTexture.new()
-var blood_image: Image = Image.new()
 
 var dynamic_blood: Resource = preload("res://scenes/gfx/DynamicBlood.tscn")
 
@@ -11,37 +8,29 @@ const SURFACE_IMG_WIDTH = 1500
 const SURFACE_IMG_HEIGHT = 1000
 
 func _ready() -> void:
-	#create our surface image and display it
+	texture = ImageTexture.new()
 	surface_image.create(SURFACE_IMG_WIDTH, SURFACE_IMG_HEIGHT, false, Image.FORMAT_RGBAH)
 	surface_image.fill(Color(0,0,0,0))
-	surface_texture.create_from_image(surface_image, 4)
-	
-	#We do this once, instead of every single time in blood objects
-	blood_image.load("res://assets/img/gfx/blood.png")
-	blood_image.convert(Image.FORMAT_RGBAH)
-	
-	texture = surface_texture
-	
-var a = true
+	texture.create_from_image(surface_image)
 	
 func draw_blood(draw_pos: Vector2) -> void:
-	if not a: return
-	surface_image.blit_rect(blood_image,
-							Rect2(Vector2(0,0), Vector2(1,1)),
-							draw_pos)
+	surface_image.lock()
+	surface_image.set_pixel(draw_pos.x, draw_pos.y, Color(1,0,0,1))
+	surface_image.unlock()
 
 func remove_blood(rect: Rect2) -> void:
-	a = false
-	surface_image.fill_rect(Rect2(Vector2(rect.position.x+SURFACE_IMG_WIDTH/2, 
-							rect.position.y+SURFACE_IMG_HEIGHT/2), 
-							rect.size), 
-							Color(1,1,1,0))
+	surface_image.lock()
+	for i in rect.size.x:
+		for j in rect.size.y:
+			surface_image.set_pixel(rect.position.x+SURFACE_IMG_WIDTH/2+i,
+			 rect.position.y+SURFACE_IMG_HEIGHT/2+j, Color(1,1,1,1))
+	surface_image.unlock()
 	
 func clear_surface() -> void:
 	surface_image.fill(Color(0,0,0,0))
 	
 func _physics_process(delta: float) -> void:
-	surface_texture.create_from_image(surface_image)
+	texture.create_from_image(surface_image)
 	
 	if (Input.is_action_pressed("mb_left")):
 		for i in range(1):
